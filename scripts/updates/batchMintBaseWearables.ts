ddb29ae23dff5e12429e02f0971d419539eb509e
Timidan <akinnusotutemitayodaniel@gmail.com>
2025-08-12 08:15:14 +0100

    mint sche matics

---
+++ scripts/updates/batchMintBaseWearables.ts
@@ -0,0 +1,103 @@
+import { ethers, network } from "hardhat";
+import {
+  gasPrice,
+  itemManagerAlt,
+  maticForgeDiamond,
+} from "../helperFunctions";
+import { ForgeFacet, ForgeTokenFacet } from "../../typechain";
+import { LedgerSigner } from "@anders-t/ethers-ledger";
+import { varsForNetwork } from "../../helpers/constants";
+
+export async function batchMintBaseWearables() {
+  const c = await varsForNetwork(ethers);
+  let signer;
+  const testing = ["hardhat", "localhost"].includes(network.name);
+  if (testing) {
+    const ownershipFacet = await ethers.getContractAt(
+      "OwnershipFacet",
+      c.forgeDiamond!
+    );
+    const owner = await ownershipFacet.owner();
+
+    console.log("current owner:", owner);
+
+    await network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [owner],
+    });
+    signer = await ethers.provider.getSigner(owner);
+  } else if (network.name === "matic") {
+    //item manager - ledger
+    signer = new LedgerSigner(ethers.provider, "m/44'/60'/1'/0/0");
+  } else throw Error("Incorrect network selected");
+
+  let forgeFacet = (await ethers.getContractAt(
+    "contracts/Aavegotchi/ForgeDiamond/facets/ForgeFacet.sol:ForgeFacet",
+    c.forgeDiamond!,
+    signer
+  )) as ForgeFacet;
+
+  // schematics
+  const common = [418];
+  // const uncommon = [373, 377];
+  const rare = [419];
+  const legendary = [420];
+  // const mythical = [384];
+  // const godlike = [385, 386, 387];
+  const ids = [common, rare, legendary];
+  const totalAmounts = [1000, 250, 100];
+
+  // const percents = [0.4, 0.6];
+  // const receipients = [itemManagerAlt, c.forgeDiamond!];
+
+  // for (let j = 0; j < receipients.length; j++) {
+  //   const transferAmount = [];
+  //   const transferIds = [];
+
+  //   const recipient = receipients[j];
+  //   const percent = percents[j];
+
+  //   for (let i = 0; i < ids.length; i++) {
+  //     if (totalAmounts[i] === 0) {
+  //       continue;
+  //     }
+  //     for (let j = 0; j < ids[i].length; j++) {
+  //       transferIds.push(ids[i][j]);
+  //       transferAmount.push(totalAmounts[i] * percent);
+  //     }
+  //   }
+
+  //   console.log(
+  //     `Batch minting to ${recipient}: ${transferIds} ${transferAmount}`
+  //   );
+
+  //   const tx = await forgeFacet.adminMintBatch(
+  //     recipient,
+  //     transferIds,
+  //     transferAmount,
+  //     { gasPrice: gasPrice }
+  //   );
+  //   console.log("tx hash:", tx.hash);
+  //   const receipt = await tx.wait();
+  //   if (!receipt.status) {
+  //     throw Error(`Error with transaction: ${tx.hash}`);
+  //   }
+
+  //   let forgeTokenFacet = (await ethers.getContractAt(
+  //     "contracts/Aavegotchi/ForgeDiamond/facets/ForgeTokenFacet.sol:ForgeTokenFacet",
+  //     maticForgeDiamond,
+  //     signer
+  //   )) as ForgeTokenFacet;
+  //   const balance = await forgeTokenFacet.balanceOfOwner(recipient);
+  //   console.log("balance", balance);
+  // }
+}
+
+if (require.main === module) {
+  batchMintBaseWearables()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
